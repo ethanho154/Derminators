912ae6a537c0e84e3d687b12c15deacd01a0e054
Gina Lee <gi.na.lee@duke.edu>
2019-03-01 02:01:50 -0500

    for ethan to try running photo database activity specifically

--- app/src/main/java/ginalee0122/github/com/myapplication/PhotoDatabseActivity.java
+++ app/src/main/java/ginalee0122/github/com/myapplication/PhotoDatabseActivity.java
@@ -3,6 +3,7 @@ package ginalee0122.github.com.myapplication;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -14,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 
 
+
 public class PhotoDatabseActivity extends AppCompatActivity {
 
   @Override
@@ -21,19 +23,23 @@ public class PhotoDatabseActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_photo_databse);
     StorageReference mStorageRef;
-    mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://bme590project.appspot.com/webcam/cam1-2019-03-01-015451.jpg");
+    mStorageRef = FirebaseStorage.getInstance().getReference();
+    //FromUrl("gs://bme590project.appspot.com/webcam/cam1-2019-03-01-015451.jpg");
+    Button button = (Button) findViewById(R.id.floatingActionButton);
     try {
       File localFile = File.createTempFile("images", "jpg");
       mStorageRef.getFile(localFile)
         .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-          @Override
+
           public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+            System.out.println("successfully download the byte blob!");
             // Successfully downloaded data to local file
             // ...
           }
         }).addOnFailureListener(new OnFailureListener() {
         @Override
         public void onFailure(@NonNull Exception exception) {
+          System.out.println("F A I L :P");
           // Handle failed download
           // ...
         }

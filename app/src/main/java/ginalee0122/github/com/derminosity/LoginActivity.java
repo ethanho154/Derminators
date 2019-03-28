c033cbb8e60c0a2670df700adbb11ed0fe3253dd
Ethan Ho <eh154@duke.edu>
2019-03-28 01:06:56 -0400

    Simplified login menu activity by commenting out unused methods (temporary)

--- app/src/main/java/ginalee0122/github/com/derminosity/LoginActivity.java
+++ app/src/main/java/ginalee0122/github/com/derminosity/LoginActivity.java
@@ -4,28 +4,14 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,15 +24,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
+//public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, OnClickListener {
 
   FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
@@ -56,7 +38,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
    * Keep track of the login task to ensure we can cancel it if requested.
    */
 //  private UserLoginTask mAuthTask = null;
-
 
   // UI references.
   private AutoCompleteTextView mEmailView;
@@ -70,88 +51,86 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
 
-//    FirebaseUser currentUser = mAuth.getCurrentUser();
     // Set up the login form.
     mEmailView = findViewById(R.id.email);
-    populateAutoComplete();
-
+//    populateAutoComplete();
     mPasswordView = findViewById(R.id.password);
-    mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-      @Override
-      public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-        if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-          attemptLogin();
-          return true;
-        }
-        return false;
-      }
-    });
+//    mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//      @Override
+//      public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//        if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+//          validateFields();
+//          return true;
+//        }
+//        return false;
+//      }
+//    });
 
     Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
-    mEmailSignInButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        attemptLogin();
-      }
-    });
+    mEmailSignInButton.setOnClickListener(this);
+//    mEmailSignInButton.setOnClickListener(new OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        validateFields();
+//      }
+//    });
 
     mLoginFormView = findViewById(R.id.login_form);
     mProgressView = findViewById(R.id.login_progress);
 
     signUpView = findViewById(R.id.sign_up_text);
-    initializeSignUp();
+//    initializeSignUp();
   }
 
-  private void populateAutoComplete() {
-    if (!mayRequestContacts()) {
-      return;
-    }
-
-    getLoaderManager().initLoader(0, null, this);
-  }
-
-  private boolean mayRequestContacts() {
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-      return true;
-    }
-    if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-      return true;
-    }
-    if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-      Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-        .setAction(android.R.string.ok, new View.OnClickListener() {
-          @Override
-          @TargetApi(Build.VERSION_CODES.M)
-          public void onClick(View v) {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-          }
-        });
-    } else {
-      requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-    }
-    return false;
-  }
-
-  /**
-   * Callback received when a permissions request has been completed.
-   */
-  @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                         @NonNull int[] grantResults) {
-    if (requestCode == REQUEST_READ_CONTACTS) {
-      if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        populateAutoComplete();
-      }
-    }
-  }
-
+//  private void populateAutoComplete() {
+//    if (!mayRequestContacts()) {
+//      return;
+//    }
+//
+//    getLoaderManager().initLoader(0, null, this);
+//  }
+//
+//  private boolean mayRequestContacts() {
+//    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+//      return true;
+//    }
+//    if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+//      return true;
+//    }
+//    if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+//      Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+//        .setAction(android.R.string.ok, new View.OnClickListener() {
+//          @Override
+//          @TargetApi(Build.VERSION_CODES.M)
+//          public void onClick(View v) {
+//            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+//          }
+//        });
+//    } else {
+//      requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+//    }
+//    return false;
+//  }
+//
+//  /**
+//   * Callback received when a permissions request has been completed.
+//   */
+//  @Override
+//  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+//                                         @NonNull int[] grantResults) {
+//    if (requestCode == REQUEST_READ_CONTACTS) {
+//      if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//        populateAutoComplete();
+//      }
+//    }
+//  }
 
   /**
    * Attempts to sign in or register the account specified by the login form.
    * If there are form errors (invalid email, missing fields, etc.), the
    * errors are presented and no actual login attempt is made.
    */
-  private void attemptLogin() {
+  private boolean validateFields(String email, String password) {
 //    if (mAuthTask != null) {
 //      return;
 //    }
@@ -161,14 +140,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     mPasswordView.setError(null);
 
     // Store values at the time of the login attempt.
-    String email = mEmailView.getText().toString();
-    String password = mPasswordView.getText().toString();
+//    String email = mEmailView.getText().toString();
+//    String password = mPasswordView.getText().toString();
 
     boolean cancel = false;
     View focusView = null;
 
     // Check for a valid password, if the user entered one.
-    if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+      if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
       mPasswordView.setError(getString(R.string.error_invalid_password));
       focusView = mPasswordView;
       cancel = true;
@@ -186,35 +165,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     if (cancel) {
-      // There was an error; don't attempt login and focus the first
-      // form field with an error.
-      focusView.requestFocus();
-    } else {
-      // Show a progress spinner, and kick off a background task to
-      // perform the user login attempt.
-      showProgress(true);
-        mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d("Login attempt", "signInWithEmail:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-//                        updateUI(user);
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("Login attempt", "signInWithEmail:failure", task.getException());
-                        Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show();
-//                        updateUI(null);
-                    }
-                    showProgress(false);
-                }
-            });
+        focusView.requestFocus();
+    }
+
+    return !cancel;
+
+//    if (cancel) {
+//      // There was an error; don't attempt login and focus the first
+//      // form field with an error.
+//      focusView.requestFocus();
+//    } else {
+
 //      mAuthTask = new UserLoginTask(email, password);
 //      mAuthTask.execute((Void) null);
-    }
+//    }
   }
 
   private boolean isEmailValid(String email) {
@@ -225,6 +189,38 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
   private boolean isPasswordValid(String password) {
     //TODO: Replace this with your own logic
     return password.length() > 4;
+  }
+
+  private void signIn() {
+      String email = mEmailView.getText().toString();
+      String password = mPasswordView.getText().toString();
+
+      // Show a progress spinner, and kick off a background task to
+      // perform the user login attempt.
+      if (validateFields(email, password)) {
+          showProgress(true);
+          mAuth.signInWithEmailAndPassword(email, password)
+                  .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                      @Override
+                      public void onComplete(@NonNull Task<AuthResult> task) {
+                          if (task.isSuccessful()) {
+                              // Sign in success, update UI with the signed-in user's information
+                              Log.d("Login attempt", "signInWithEmail:success");
+                              FirebaseUser user = mAuth.getCurrentUser();
+                          } else {
+                              // If sign in fails, display a message to the user.
+                              Log.w("Login attempt", "signInWithEmail:failure", task.getException());
+                              Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                      Toast.LENGTH_SHORT).show();
+                          }
+                          showProgress(false);
+                      }
+          });
+      }
+  }
+
+  private void createAccount() {
+
   }
 
   /**
@@ -263,67 +259,75 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
   }
 
-  @Override
-  public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-    return new CursorLoader(this,
-      // Retrieve data rows for the device user's 'profile' contact.
-      Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+//  @Override
+//  public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+//    return new CursorLoader(this,
+//      // Retrieve data rows for the device user's 'profile' contact.
+//      Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+//        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+//
+//      // Select only email addresses.
+//      ContactsContract.Contacts.Data.MIMETYPE +
+//        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+//      .CONTENT_ITEM_TYPE},
+//
+//      // Show primary email addresses first. Note that there won't be
+//      // a primary email address if the user hasn't specified one.
+//      ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+//  }
+//
+//  @Override
+//  public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+//    List<String> emails = new ArrayList<>();
+//    cursor.moveToFirst();
+//    while (!cursor.isAfterLast()) {
+//      emails.add(cursor.getString(ProfileQuery.ADDRESS));
+//      cursor.moveToNext();
+//    }
+//
+//    addEmailsToAutoComplete(emails);
+//  }
+//
+//  @Override
+//  public void onLoaderReset(Loader<Cursor> cursorLoader) {
+//
+//  }
+//
+//  private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+//    //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+//    ArrayAdapter<String> adapter =
+//      new ArrayAdapter<>(LoginActivity.this,
+//        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+//
+//    mEmailView.setAdapter(adapter);
+//  }
 
-      // Select only email addresses.
-      ContactsContract.Contacts.Data.MIMETYPE +
-        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-      .CONTENT_ITEM_TYPE},
-
-      // Show primary email addresses first. Note that there won't be
-      // a primary email address if the user hasn't specified one.
-      ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-  }
-
-  @Override
-  public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-    List<String> emails = new ArrayList<>();
-    cursor.moveToFirst();
-    while (!cursor.isAfterLast()) {
-      emails.add(cursor.getString(ProfileQuery.ADDRESS));
-      cursor.moveToNext();
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.email_sign_in_button) {
+            signIn();
+        }
     }
 
-    addEmailsToAutoComplete(emails);
-  }
 
-  @Override
-  public void onLoaderReset(Loader<Cursor> cursorLoader) {
+//    private interface ProfileQuery {
+//    String[] PROJECTION = {
+//      ContactsContract.CommonDataKinds.Email.ADDRESS,
+//      ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+//    };
+//
+//    int ADDRESS = 0;
+//    int IS_PRIMARY = 1;
+//  }
 
-  }
-
-  private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-    //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-    ArrayAdapter<String> adapter =
-      new ArrayAdapter<>(LoginActivity.this,
-        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-    mEmailView.setAdapter(adapter);
-  }
-
-
-  private interface ProfileQuery {
-    String[] PROJECTION = {
-      ContactsContract.CommonDataKinds.Email.ADDRESS,
-      ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-    };
-
-    int ADDRESS = 0;
-    int IS_PRIMARY = 1;
-  }
-
-  private void initializeSignUp() {
-      signUpView.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
-          }
-      });
-  }
+//  private void initializeSignUp() {
+//      signUpView.setOnClickListener(new View.OnClickListener() {
+//          @Override
+//          public void onClick(View v) {
+//              startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+//          }
+//      });
+//  }
 }
 

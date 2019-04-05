9de28daabf76af3ba26ed228e568f1cbc957f634
Ethan Ho <eh154@duke.edu>
2019-04-05 02:39:18 -0400

    Tested custom imageview class which would allow for drawing and annotations on image

--- app/src/main/java/derminators/github/com/derminosity/MarkImageActivity.java
+++ app/src/main/java/derminators/github/com/derminosity/MarkImageActivity.java
@@ -1,13 +1,29 @@
 package derminators.github.com.derminosity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class MarkImageActivity extends AppCompatActivity {
+    private AnnotateView annotateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mark_image);
+        setAnnotateView();
+    }
+
+    private void populateActivity() {
+
+    }
+
+    private void setAnnotateView() {
+//        annotateView = new AnnotateView();
+    annotateView = findViewById(R.id.annotateView);
+    Glide.with(MarkImageActivity.this)
+        .load(this.getFilesDir().getAbsolutePath() + "/cam2.jpg")
+        .into(annotateView);
     }
 }

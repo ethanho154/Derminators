c58f5f1559e83ed376db1a6cabc5cdf65c7ac78e
Ethan Ho <eh154@duke.edu>
2019-04-11 01:18:38 -0400

    Can now add patient users

--- app/src/main/java/derminators/github/com/derminosity/SignUpActivity.java
+++ app/src/main/java/derminators/github/com/derminosity/SignUpActivity.java
@@ -5,9 +5,9 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -18,14 +18,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
@@ -143,6 +140,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         final String lastName = lastNameView.getText().toString();
         final String email = emailView.getText().toString();
         String password = passwordView.getText().toString();
+        final String userStatus = userType.getSelectedItem().toString();
+
         final String hospital = hospitalView.getText().toString();
 
         if (validateFields(firstName, lastName, email, password, hospital)) {
@@ -156,7 +155,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                         currentUser = mAuth.getCurrentUser();
                         String uid = currentUser.getUid();
                         Log.d("Signup attempt", "saving data model to firebase");
-                        saveData(uid, email, firstName, lastName, hospital);
+                        saveData(uid, email, firstName, lastName, userStatus, hospital);
                     }
                 })
                 .addOnFailureListener(this, new OnFailureListener() {
@@ -208,14 +207,15 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    private void saveData(String uid, String email, String firstName, String lastName, String hospital) {
-        Doctor doctor = new Doctor(uid, firstName, lastName, hospital);
-        db.collection("doctors").document(email)
+    private void saveData(String uid, String email, String firstName, String lastName, String userStatus, String hospital) {
+        if (userStatus.equals("Doctor")) {
+            Doctor doctor = new Doctor(uid, firstName, lastName, hospital);
+            db.collection("doctors").document(email)
                 .set(doctor)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d("saveData", "DocumentSnapshot successfully written!");
+                        Log.d("saveData", "Doctor successfully written!");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -224,6 +224,24 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                         Log.w("saveData", "Error writing document", e);
                     }
                 });
+        } else {
+            Patient patient = new Patient(uid, firstName, lastName);
+            db.collection("patients").document(email)
+                    .set(patient)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("saveData", "Patient successfully written!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("saveData", "Error writing document", e);
+                        }
+                    });
+        }
+
     }
 
     @Override

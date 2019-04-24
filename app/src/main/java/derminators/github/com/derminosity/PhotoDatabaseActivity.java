3045cc1f46558b4ea0d1e530018b6183e1f45cab
Ethan Ho <eh154@duke.edu>
2019-04-24 00:51:13 -0400

    turned imageviews into a single listview which can load all images and let user scroll through them

--- app/src/main/java/derminators/github/com/derminosity/PhotoDatabaseActivity.java
+++ app/src/main/java/derminators/github/com/derminosity/PhotoDatabaseActivity.java
@@ -1,5 +1,6 @@
 package derminators.github.com.derminosity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,12 +8,15 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.module.AppGlideModule;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,18 +27,23 @@ import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoDatabaseActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     private StorageReference mStorageRef;
     private AppGlideModule glideModule;
 
-    private ImageView top;
-    private ImageView mid;
-    private ImageView bot;
+    private ListView listView;
+    private List<String> images;
 
-    File directory;
-    File file = null;
+//    private ImageView top;
+//    private ImageView mid;
+//    private ImageView bot;
+
+//    File directory;
+//    File file = null;
 
     private AlertDialog.Builder builder;
 
@@ -43,8 +52,8 @@ public class PhotoDatabaseActivity extends AppCompatActivity implements View.OnC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_database);
 
-        directory = this.getFilesDir();
-        Log.d("File path", directory.getAbsolutePath());
+//        directory = this.getFilesDir();
+//        Log.d("File path", directory.getAbsolutePath());
 
         initializeViews();
 
@@ -66,33 +75,31 @@ public class PhotoDatabaseActivity extends AppCompatActivity implements View.OnC
 
             @Override
             public void onClick(View v) {
-                Log.d("please work", "button clicked");
+        Log.d("please work", "button clicked");
 
-                Log.d("mStorageRef", "detected!");
+        Log.d("mStorageRef", "detected!");
 
-                downloadImage(mStorageRef);
+        downloadImage(mStorageRef);
 
-                try {
-                File localFile = File.createTempFile("images", "jpg");
-                mStorageRef.getFile(localFile)
-                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-
-                  public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+        try {
+            File localFile = File.createTempFile("images", "jpg");
+            mStorageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                     System.out.println("successfully download the byte blob!");
-                    // Successfully downloaded data to local file
-                    // ...
-                  }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                      System.out.println("F A I L :P");
-                      // Handle failed download
-                      // ...
-                    }
-                });
-                } catch (IOException e) {
-                  e.printStackTrace();
+            // Successfully downloaded data to local file
+            // ...
                 }
+            }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                System.out.println("F A I L :P");
+              // Handle failed download
+              // ...
+                }
+        });
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
               }
         });
     }
@@ -114,47 +121,55 @@ public class PhotoDatabaseActivity extends AppCompatActivity implements View.OnC
             });
 
 
-        top = findViewById(R.id.topView);
-        mid = findViewById(R.id.midView);
-        bot = findViewById(R.id.botView);
+        listView = findViewById(R.id.listView_photoDB_images);
 
-        top.setOnClickListener(this);
-        mid.setOnClickListener(this);
-        bot.setOnClickListener(this);
+//        top = findViewById(R.id.topView);
+//        mid = findViewById(R.id.midView);
+//        bot = findViewById(R.id.botView);
+
+//        top.setOnClickListener(this);
+//        mid.setOnClickListener(this);
+//        bot.setOnClickListener(this);
     }
 
     private void downloadImage(StorageReference mStorageRef) {
-        Glide.with(PhotoDatabaseActivity.this)
-//            .load(mStorageRef.child("webcam/cam2-2019-03-01-083802.jpg"))
-            .load(mStorageRef.child("webcam/cam2.jpg"))
-            .diskCacheStrategy(DiskCacheStrategy.DATA)
-            .into(top);
-        file = new File(directory, "cam2.jpg");
-        mStorageRef.child("webcam/cam2.jpg").getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                Log.d("Save", "Success");
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("Save", "Fail");
-            }
-        });
-        for (File f : directory.listFiles()) {
-            System.out.println(f.getAbsolutePath());
-            System.out.println(f.getName());
+        images = new ArrayList<>();
+        for (int i = 1; i <= 4; i++) {
+            images.add("webcam/cam" + i + ".jpg");
         }
-        Glide.with(PhotoDatabaseActivity.this)
-//            .load(mStorageRef.child("webcam/cam1-2019-03-01-083802.jpg"))
-            .load(mStorageRef.child("webcam/cam1.jpg"))
-            .diskCacheStrategy(DiskCacheStrategy.DATA)
-            .into(mid);
-        Glide.with(PhotoDatabaseActivity.this)
-//            .load(mStorageRef.child("webcam/cam3-2019-03-01-083802.jpg"))
-            .load(mStorageRef.child("webcam/cam3.jpg"))
-            .diskCacheStrategy(DiskCacheStrategy.DATA)
-            .into(bot);
+
+        listView.setAdapter(new ImageListAdapter(PhotoDatabaseActivity.this, R.layout.list_item, images));
+//        Glide.with(PhotoDatabaseActivity.this)
+////            .load(mStorageRef.child("webcam/cam2-2019-03-01-083802.jpg"))
+//            .load(mStorageRef.child("webcam/cam2.jpg"))
+//            .diskCacheStrategy(DiskCacheStrategy.DATA)
+//            .into(top);
+//        file = new File(directory, "cam2.jpg");
+//        mStorageRef.child("webcam/cam2.jpg").getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                Log.d("Save", "Success");
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.d("Save", "Fail");
+//            }
+//        });
+//        for (File f : directory.listFiles()) {
+//            System.out.println(f.getAbsolutePath());
+//            System.out.println(f.getName());
+//        }
+//        Glide.with(PhotoDatabaseActivity.this)
+////            .load(mStorageRef.child("webcam/cam1-2019-03-01-083802.jpg"))
+//            .load(mStorageRef.child("webcam/cam1.jpg"))
+//            .diskCacheStrategy(DiskCacheStrategy.DATA)
+//            .into(mid);
+//        Glide.with(PhotoDatabaseActivity.this)
+////            .load(mStorageRef.child("webcam/cam3-2019-03-01-083802.jpg"))
+//            .load(mStorageRef.child("webcam/cam3.jpg"))
+//            .diskCacheStrategy(DiskCacheStrategy.DATA)
+//            .into(bot);
 
         Log.d("glide coming thru", "wala");
     }
@@ -163,5 +178,28 @@ public class PhotoDatabaseActivity extends AppCompatActivity implements View.OnC
     public void onClick(View v) {
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    public class ImageListAdapter extends ArrayAdapter<String> {
+        List<String> items;
+
+        public ImageListAdapter(Context context, int resource, List<String> items) {
+            super(context, resource, items);
+            this.items = items;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(R.layout.list_item, null);
+            }
+
+            Glide.with(getContext())
+                    .load(mStorageRef.child(items.get(position)))
+                    .into((ImageView) convertView);
+
+            return convertView;
+        }
     }
 }

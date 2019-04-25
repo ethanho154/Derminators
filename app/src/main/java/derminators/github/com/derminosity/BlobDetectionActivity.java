ce05264f2c3efa47be9d08f3937f03081c922687
Ethan Ho <eh154@duke.edu>
2019-04-25 02:39:16 -0400

    static image blob detection

--- app/src/main/java/derminators/github/com/derminosity/BlobDetectionActivity.java
+++ app/src/main/java/derminators/github/com/derminosity/BlobDetectionActivity.java
@@ -40,7 +40,7 @@ import android.widget.ImageView;
 import static java.security.AccessController.getContext;
 
 
-public class BlobDetectionActivity extends AppCompatActivity {
+public class BlobDetectionActivity extends AppCompatActivity implements OnTouchListener {
 
     private static final String  TAG              = "OCVSample::Activity";
 
@@ -54,8 +54,8 @@ public class BlobDetectionActivity extends AppCompatActivity {
     private Scalar               CONTOUR_COLOR;
 
     private Bitmap bitmap;
+    private Bitmap processedBitmap;
     private ImageView imageView;
-    private boolean loaded = false;
 
 //    private CameraBridgeViewBase mOpenCvCameraView;
 //    private static final int PERMISSION_REQUEST_CODE = 200;
@@ -69,8 +69,6 @@ public class BlobDetectionActivity extends AppCompatActivity {
                     Log.i(TAG, "OpenCV loaded successfully");
 
                     onImageLoaded();
-//                    mOpenCvCameraView.enableView();
-//                    mOpenCvCameraView.setOnTouchListener(BlobDetectionActivity.this);
                 } break;
                 default:
                 {
@@ -87,30 +85,17 @@ public class BlobDetectionActivity extends AppCompatActivity {
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-//
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-//            } else {
-//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
-//            }
-//
-//        }
-
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_blob_detection);
         imageView = findViewById(R.id.image_view_blob_detection_analyze);
+        imageView.setOnTouchListener(this);
 
         File file = new File(this.getFilesDir(), "savedImage.jpg");
-//        File file = new File(this.getFilesDir(), "test.jpg");
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), bmOptions);
-
-//        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
-//        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-//        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     private void onImageLoaded() {
@@ -123,22 +108,8 @@ public class BlobDetectionActivity extends AppCompatActivity {
         CONTOUR_COLOR = new Scalar(255,0,0,255);
 
         Utils.bitmapToMat(bitmap, mRgba);
-//        mRgba = inputFrame.rgba();
 
-        if (mIsColorSelected) {
-            mDetector.process(mRgba);
-            List<MatOfPoint> contours = mDetector.getContours();
-            Log.e(TAG, "Contours count: " + contours.size());
-            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
-
-            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
-            colorLabel.setTo(mBlobColorRgba);
-
-            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
-            mSpectrum.copyTo(spectrumLabel);
-        }
-
-        Bitmap processedBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        processedBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
 
         Utils.matToBitmap(mRgba, processedBitmap);
         imageView.setImageBitmap(processedBitmap);
@@ -148,8 +119,6 @@ public class BlobDetectionActivity extends AppCompatActivity {
     public void onPause()
     {
         super.onPause();
-//        if (mOpenCvCameraView != null)
-//            mOpenCvCameraView.disableView();
     }
 
     @Override
@@ -167,8 +136,6 @@ public class BlobDetectionActivity extends AppCompatActivity {
 
     public void onDestroy() {
         super.onDestroy();
-//        if (mOpenCvCameraView != null)
-//            mOpenCvCameraView.disableView();
     }
 
 
@@ -180,93 +147,73 @@ public class BlobDetectionActivity extends AppCompatActivity {
         return new Scalar(pointMatRgba.get(0, 0));
     }
 
-//    @Override
-//    public boolean onTouch(View v, MotionEvent event) {
-//        int cols = mRgba.cols();
-//        int rows = mRgba.rows();
-//
-//        int xOffset = (mOpenCvCameraView.getWidth() - cols) / 2;
-//        int yOffset = (mOpenCvCameraView.getHeight() - rows) / 2;
-//
-//        int x = (int)event.getX() - xOffset;
-//        int y = (int)event.getY() - yOffset;
-//
-//        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
-//
-//        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
-//
-//        Rect touchedRect = new Rect();
-//
-//        touchedRect.x = (x>4) ? x-4 : 0;
-//        touchedRect.y = (y>4) ? y-4 : 0;
-//
-//        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
-//        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
-//
-//        Mat touchedRegionRgba = mRgba.submat(touchedRect);
-//
-//        Mat touchedRegionHsv = new Mat();
-//        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
-//
-//        // Calculate average color of touched region
-//        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
-//        int pointCount = touchedRect.width*touchedRect.height;
-//        for (int i = 0; i < mBlobColorHsv.val.length; i++)
-//            mBlobColorHsv.val[i] /= pointCount;
-//
-//        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
-//
-//        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
-//                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
-//
-//        mDetector.setHsvColor(mBlobColorHsv);
-//
-//        // in the source code, the interpolation was Imgproc.INTER_LINEAR
-//        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE, 0, 0, Imgproc.INTER_LINEAR);
-//
-//        mIsColorSelected = true;
-//
-//        touchedRegionRgba.release();
-//        touchedRegionHsv.release();
-//
-//        return false; // don't need subsequent touch events
-//    }
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Utils.bitmapToMat(bitmap, mRgba);
 
-//    @Override
-//    public void onCameraViewStarted(int width, int height) {
-//        mRgba = new Mat(height, width, CvType.CV_8UC4);
-//        mDetector = new BlobDetector();
-//        mSpectrum = new Mat();
-//        mBlobColorRgba = new Scalar(255);
-//        mBlobColorHsv = new Scalar(255);
-//        SPECTRUM_SIZE = new Size(200, 64);
-//        CONTOUR_COLOR = new Scalar(255,0,0,255);
-//
-//    }
-//
-//    @Override
-//    public void onCameraViewStopped() {
-//        mRgba.release();
-//
-//    }
-//
-//    @Override
-//    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-//        mRgba = inputFrame.rgba();
-//
-//        if (mIsColorSelected) {
-//            mDetector.process(mRgba);
-//            List<MatOfPoint> contours = mDetector.getContours();
-//            Log.e(TAG, "Contours count: " + contours.size());
-//            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
-//
-//            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
-//            colorLabel.setTo(mBlobColorRgba);
-//
-//            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
-//            mSpectrum.copyTo(spectrumLabel);
-//        }
-//
-//        return mRgba;
-//    }
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+
+        int xOffset = (imageView.getWidth() - cols) / 2;
+        int yOffset = (imageView.getHeight() - rows) / 2;
+
+        int x = (int)event.getX() - xOffset;
+        int y = (int)event.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x>4) ? x-4 : 0;
+        touchedRect.y = (y>4) ? y-4 : 0;
+
+        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width*touchedRect.height;
+        for (int i = 0; i < mBlobColorHsv.val.length; i++)
+            mBlobColorHsv.val[i] /= pointCount;
+
+        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
+
+        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
+                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
+
+        mDetector.setHsvColor(mBlobColorHsv);
+
+        // in the source code, the interpolation was Imgproc.INTER_LINEAR
+        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE, 0, 0, Imgproc.INTER_LINEAR);
+
+        mIsColorSelected = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        if (mIsColorSelected) {
+            mDetector.process(mRgba);
+            List<MatOfPoint> contours = mDetector.getContours();
+            Log.e(TAG, "Contours count: " + contours.size());
+            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+            colorLabel.setTo(mBlobColorRgba);
+
+            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+            mSpectrum.copyTo(spectrumLabel);
+        }
+
+        Utils.matToBitmap(mRgba, processedBitmap);
+        imageView.setImageBitmap(processedBitmap);
+
+        return false; // don't need subsequent touch events
+    }
 }
